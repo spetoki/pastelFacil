67027bb16934e884daed54188c64f029fe88be52
Irineu Marcos <spetoki@gmail.com>
2025-09-24 07:02:40 +0000

    deixar a cor do app escura

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR" suppressHydrationWarning>
+    <html lang="pt-BR" className="dark" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
